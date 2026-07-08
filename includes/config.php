a39970c056eb83c7b0886463285b04fba7df00e9
gamithhewathenna-cyber <gamithhewathenna@gmail.com>
2026-07-08 15:26:58 +0530

    Image eizes

--- includes/config.php
+++ includes/config.php
@@ -48,6 +48,60 @@ function sanitizeBr($input) {
     return preg_replace('/&lt;br\s*\/?&gt;/i', '<br>', $escaped);
 }
 
+// Resizes+crops an uploaded image to exact target dimensions (cover-fit, no distortion).
+function resizeCoverCrop($srcPath, $destPath, $targetW, $targetH, $ext) {
+    if (!extension_loaded('gd')) return false;
+
+    $size = @getimagesize($srcPath);
+    if (!$size) return false;
+    [$srcW, $srcH] = $size;
+
+    switch ($ext) {
+        case 'jpg': case 'jpeg': $srcImg = @imagecreatefromjpeg($srcPath); break;
+        case 'png':              $srcImg = @imagecreatefrompng($srcPath); break;
+        case 'webp':              $srcImg = @imagecreatefromwebp($srcPath); break;
+        case 'gif':               $srcImg = @imagecreatefromgif($srcPath); break;
+        default: return false;
+    }
+    if (!$srcImg) return false;
+
+    $srcRatio    = $srcW / $srcH;
+    $targetRatio = $targetW / $targetH;
+    if ($srcRatio > $targetRatio) {
+        $cropH = $srcH;
+        $cropW = (int) round($srcH * $targetRatio);
+        $cropX = (int) round(($srcW - $cropW) / 2);
+        $cropY = 0;
+    } else {
+        $cropW = $srcW;
+        $cropH = (int) round($srcW / $targetRatio);
+        $cropX = 0;
+        $cropY = (int) round(($srcH - $cropH) / 2);
+    }
+
+    $dstImg = imagecreatetruecolor($targetW, $targetH);
+    if (in_array($ext, ['png', 'webp', 'gif'])) {
+        imagealphablending($dstImg, false);
+        imagesavealpha($dstImg, true);
+        $transparent = imagecolorallocatealpha($dstImg, 0, 0, 0, 127);
+        imagefilledrectangle($dstImg, 0, 0, $targetW, $targetH, $transparent);
+    }
+
+    imagecopyresampled($dstImg, $srcImg, 0, 0, $cropX, $cropY, $targetW, $targetH, $cropW, $cropH);
+
+    $ok = false;
+    switch ($ext) {
+        case 'jpg': case 'jpeg': $ok = imagejpeg($dstImg, $destPath, 90); break;
+        case 'png':               $ok = imagepng($dstImg, $destPath); break;
+        case 'webp':              $ok = imagewebp($dstImg, $destPath, 90); break;
+        case 'gif':               $ok = imagegif($dstImg, $destPath); break;
+    }
+
+    imagedestroy($srcImg);
+    imagedestroy($dstImg);
+    return $ok;
+}
+
 function isLoggedIn() {
     return isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true;
 }
